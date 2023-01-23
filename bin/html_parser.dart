abf35a4e560fa61a49eb6574fe329467edfa33f4
SuperLiveYou <2143026883@qq.com>
2023-01-23 19:43:39 +0800

    提交

--- bin/html_parser.dart
+++ bin/html_parser.dart
@@ -4,7 +4,7 @@ void main(List<String> arguments) async {
   var test = Lexical.formString("""
     <div id="js-global-screen-reader-notice" class="sr-only" aria-live="polite"></div>
 
-
+ 
 """);
   for (;;) {
     Token token = test.getToken();
